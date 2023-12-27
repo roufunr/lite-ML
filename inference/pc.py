66cd99ee5789f2b684256ce318b38a086aacc536
roufunr <rouf@nevada.unr.edu>
2023-12-27 18:03:26 -0500

    chnaged some code in pc inference

--- inference/pc.py
+++ inference/pc.py
@@ -10,16 +10,20 @@ import time
 import logging
 import sys
 
+home_path = os.path.expanduser('~')
+root_path = os.path.abspath('./')
+# /Users/abdurrouf/Documents/lite-ML/inference
 
 logging.basicConfig(level=logging.INFO,  
                     format='%(asctime)s - %(levelname)s - %(message)s',
-                    filename='/home/rouf-linux/lite-ML/infer.log',  # Specify the log file
+                    filename= root_path + '/logger/infer_'+ str(time.time()) +'.log',
                     filemode='a') 
 
 logger = logging.getLogger(__name__)
 
 
-dataset_path = '/home/rouf-linux/TensorflowLiteData/F/'
+dataset_path = home_path + '/TensorflowLiteData/F/'
+
 total_device = 23
 def load_json_to_dict(json_path):
     with open(json_path, 'r') as file:
@@ -27,8 +31,8 @@ def load_json_to_dict(json_path):
     return json_data
 
 def load_test_data():
-    device_mapper = load_json_to_dict("/home/rouf-linux/lite-ML/device_mapper.json")
-    test_sample_idx = [x for x in range(16, 21)]
+    device_mapper = load_json_to_dict(home_path + "/lite-ML/device_mapper.json")
+    test_sample_idx = [x for x in range(1, 20 + 1)]     # for testing accuracy change range from 16 to 20 + 1
     X = []
     Y = []
     for device_id in device_mapper:
@@ -47,8 +51,8 @@ def load_test_data():
     X = scaler.fit_transform(X)
     return X, Y
 
-def run_prediction(X, Y, model_idx):
-    tf_model = tf.keras.models.load_model("/home/rouf-linux/lite-ML/models/tf/" + str(model_idx))
+def run_inference_on_tf_model(X, Y, model_idx):
+    tf_model = tf.keras.models.load_model(home_path + "/lite-ML/models/tf/" + str(model_idx))
     predictions = tf_model.predict(X)
 
     one_hot_predictions = []
@@ -71,8 +75,8 @@ def run_prediction(X, Y, model_idx):
 
     return precision, recall, f1
 
-def run_inference_lite(X, Y, model_idx):
-    interpreter = tf.lite.Interpreter(model_path="/home/rouf-linux/lite-ML/models/lite/" + str(model_idx) + ".tflite")
+def run_inference_on_lite_model(X, Y, model_idx):
+    interpreter = tf.lite.Interpreter(model_path= home_path + "/lite-ML/models/lite/" + str(model_idx) + ".tflite")
     interpreter.allocate_tensors()
     input_tensor_index = interpreter.get_input_details()[0]['index']
     predictions = np.array([])
@@ -84,10 +88,8 @@ def run_inference_lite(X, Y, model_idx):
         if predictions.size == 0:
             predictions = np.array([prediction[0]])
         else:
-            # If 'a' is not empty, use numpy.vstack to stack the new row
             predictions = np.vstack((predictions, prediction[0]))
     
-    print(predictions.shape)
     one_hot_predictions = []
     for prediction in predictions:
         one_hot_prediction = np.zeros_like(prediction)
@@ -97,38 +99,59 @@ def run_inference_lite(X, Y, model_idx):
 
     predicted_labels = one_hot_predictions
 
-    # Accuracy
+
     accuracy = metrics.accuracy_score(Y, predicted_labels)
     print(f'Accuracy: {accuracy}')
 
-    # Precision, Recall, F1-score (for each class in a multi-class setting)
     precision = metrics.precision_score(Y, predicted_labels, average='micro')  # Change average to 'micro', 'macro', or 'weighted' for multi-class
     recall = metrics.recall_score(Y, predicted_labels, average='micro')
     f1 = metrics.f1_score(Y, predicted_labels, average='micro')
 
     return precision, recall, f1
 
-s_time = time.time()
-X, Y = load_test_data()
-csv_report = []
-start_model = int(sys.argv[1])
-end_model = int(sys.argv[2])
-for i in range(start_model, end_model + 1):
-    precision_tf, recall_tf, f1_tf = run_prediction(X,Y, i)
-    precision_lite, recall_lite, f1_lite = run_inference_lite(X,Y, i)
-    csv_report.append([i, precision_tf, precision_lite, recall_tf, recall_lite, f1_tf, f1_lite])
-    logger.info("DONE ::: " + str(i))
 
-file_path = 'metric.csv'
 
-# Writing the 2D list to a CSV file
-with open(file_path, 'a', newline='') as csvfile:
-    csv_writer = csv.writer(csvfile)
-    csv_writer.writerows(csv_report)
 
-print(f"Data has been written to {file_path}")
-e_time = time.time()
-logger.info(str(start_model) + " to " + str(end_model) + " takes " + str(e_time - s_time) + " seconds")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# s_time = time.time()
+# X, Y = load_test_data()
+# csv_report = []
+# start_model = int(sys.argv[1])
+# end_model = int(sys.argv[2])
+# for i in range(start_model, end_model + 1):
+#     precision_tf, recall_tf, f1_tf = run_prediction(X,Y, i)
+#     precision_lite, recall_lite, f1_lite = run_inference_lite(X,Y, i)
+#     csv_report.append([i, precision_tf, precision_lite, recall_tf, recall_lite, f1_tf, f1_lite])
+#     logger.info("DONE ::: " + str(i))
+
+# file_path = 'metric.csv'
+
+# # Writing the 2D list to a CSV file
+# with open(file_path, 'a', newline='') as csvfile:
+#     csv_writer = csv.writer(csvfile)
+#     csv_writer.writerows(csv_report)
+
+# print(f"Data has been written to {file_path}")
+# e_time = time.time()
+# logger.info(str(start_model) + " to " + str(end_model) + " takes " + str(e_time - s_time) + " seconds")
 
     
 
