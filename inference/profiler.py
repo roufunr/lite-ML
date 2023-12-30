bfd8225eff890bd23cae2adb7a7178df6c318dd1
roufunr <rouf@nevada.unr.edu>
2023-12-30 01:30:13 -0800

    added inference time to profiler

--- inference/profiler.py
+++ inference/profiler.py
@@ -8,6 +8,8 @@ import numpy as np
 import tensorflow as tf
 import logging
 import sys
+import csv
+import time
 
 home_path = os.path.expanduser('~')
 root_path = os.path.abspath('./')
@@ -45,13 +47,12 @@ def load_data():
     return X, Y
 X, Y = load_data()
 
-
-
 def profile_line(func, *args, **kwargs):
     profiler = LineProfiler()
     profiler.add_function(func)
     profiler.runcall(func, *args, **kwargs)
     profiler.print_stats()
+
 def measure_on_tf_model():
     total_data_points = len(Y)
     for i in range(total_data_points):
@@ -64,7 +65,6 @@ def measure_on_lite_model():
     for i in range(total_data_points):
         interpreter.set_tensor(input_tensor_index, np.array([X[i]]).astype(np.float32))
         interpreter.invoke()
-
 
 def main():
     profile_line(measure_on_tf_model)
@@ -98,3 +98,104 @@ def measure_memory_on_lite_model():
 measure_memory_on_tf_model()
 measure_memory_on_lite_model()
 
+def mean(data):
+    return sum(data) / len(data)
+
+def median(data):
+    sorted_data = sorted(data)
+    n = len(sorted_data)
+    mid = n // 2
+    if n % 2 == 0:
+        return (sorted_data[mid - 1] + sorted_data[mid]) / 2
+    else:
+        return sorted_data[mid]
+
+def percentile(data, percentile):
+    size = len(data)
+    sorted_data = sorted(data)
+    index = (size * percentile) // 100
+    return sorted_data[int(index)]
+
+def variance(data):
+    data_mean = mean(data)
+    return sum((x - data_mean) ** 2 for x in data) / len(data)
+
+def skewness(data):
+    data_mean = mean(data)
+    data_variance = variance(data)
+    n = len(data)
+    return (sum((x - data_mean) ** 3 for x in data) / n) / (data_variance ** 1.5)
+
+def kurtosis(data):
+    data_mean = mean(data)
+    data_variance = variance(data)
+    n = len(data)
+    return (sum((x - data_mean) ** 4 for x in data) / n) / (data_variance ** 2) - 3
+
+def write_metrics_to_csv(metrics, file_name):
+    with open(file_name, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Metric', 'Value'])
+        for key, value in metrics.items():
+            writer.writerow([key, value])
+
+def measure_inference_time_on_tf_model():
+    total_data_points = len(Y)
+    elapsed_time = []
+    for i in range(total_data_points):
+        start_time = time.time() * 1000
+        tf_model.predict(np.array([X[i]]))
+        end_time = time.time() * 1000
+        elapsed_time.append(end_time - start_time)
+    
+    inference_times = elapsed_time
+    metrics = {
+        "average": mean(inference_times),
+        "median": median(inference_times),
+        "min": min(inference_times),
+        "max": max(inference_times),
+        "25th percentile": percentile(inference_times, 25),
+        "75th percentile": percentile(inference_times, 75),
+        "variance": variance(inference_times),
+        "range": max(inference_times) - min(inference_times),
+        "IQR": percentile(inference_times, 75) - percentile(inference_times, 25),
+        "skewness": skewness(inference_times),
+        "kurtosis": kurtosis(inference_times)
+    }
+    return metrics
+        
+def measure_inference_time_on_lite_model():
+    interpreter.allocate_tensors()
+    input_tensor_index = interpreter.get_input_details()[0]['index']
+    total_data_points = len(Y)
+    elapsed_time = []
+    for i in range(total_data_points):
+        interpreter.set_tensor(input_tensor_index, np.array([X[i]]).astype(np.float32))
+        start_time = time.time() * 1000
+        interpreter.invoke()
+        end_time = time.time() * 1000
+        elapsed_time.append(end_time - start_time)
+    
+    inference_times = elapsed_time
+    metrics = {
+        "average": mean(inference_times),
+        "median": median(inference_times),
+        "min": min(inference_times),
+        "max": max(inference_times),
+        "25th percentile": percentile(inference_times, 25),
+        "75th percentile": percentile(inference_times, 75),
+        "variance": variance(inference_times),
+        "range": max(inference_times) - min(inference_times),
+        "IQR": percentile(inference_times, 75) - percentile(inference_times, 25),
+        "skewness": skewness(inference_times),
+        "kurtosis": kurtosis(inference_times)
+    }
+    return metrics
+
+def run_infer_time_exp():
+    tf_time = measure_inference_time_on_tf_model()
+    lite_time = measure_inference_time_on_lite_model()
+    write_metrics_to_csv(tf_time, home_path + "/resource_utilization/" + model_idx + "/tf_time.csv" )
+    write_metrics_to_csv(lite_time, home_path + "/resource_utilization/" + model_idx + "/lite_time.csv")
+
+run_infer_time_exp()
