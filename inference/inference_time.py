d37377916a74eb88c5b45688ea6c61291254661a
roufunr <rouf@nevada.unr.edu>
2024-01-07 21:42:04 -0800

    added parameter for pi experiments

--- inference/inference_time.py
+++ inference/inference_time.py
@@ -173,11 +173,9 @@ def delete_model(model_idx):
     shutil.rmtree(f"{home_path}/downloaded_models/model_{model_idx}")
 
 
-def run_exp():
-    
+def run_exp(start_idx, end_idx):
     X, Y = load_data()
-    
-    for i in range(1, 13824 + 1):
+    for i in range(start_idx, end_idx + 1):
         download_model(i)
         model_idx = str(i)
         tf_model = tf.keras.models.load_model(home_path + "/downloaded_models/model_"+ str(model_idx) +"/tf")
@@ -187,10 +185,14 @@ def run_exp():
         os.makedirs(home_path + "/inference_time/" + model_idx, exist_ok=True)
         write_metrics_to_csv(tf_time, home_path + "/inference_time/" + model_idx + "/tf_time.csv" )
         write_metrics_to_csv(lite_time, home_path + "/inference_time/" + model_idx + "/lite_time.csv")
-        if i % 64 == 0:
-            logger.info(f"DONE --> : {i} model inference")
-            time.sleep(5)
+        
+        logger.info(f"DONE --> : {i} model inference")
+        delete_model(i)  
+        
 
-run_exp()
+start_idx = int(sys.argv[1])
+end_idx = int(sys.argv[2])
+
+run_exp(start_idx, end_idx)
     
 
