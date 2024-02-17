bf387dd43388fa4cd1bf8ff091e2c2287dd5f657
roufunr <rouf@nevada.unr.edu>
2024-02-16 17:10:20 -0800

    generated cpu and mem for orin and cpu

--- mlp/cpu_mem_result_parser.py
+++ mlp/cpu_mem_result_parser.py
@@ -1,45 +1,46 @@
 
 import logging
 import os
-import pandas
+import pandas as pd
 
 home_path = os.path.expanduser('~')
 
 def parse_profile(i):
-    cpu_profiler_file = f'{home_path}/mlp_profiler_result/{i}_mem.txt'
-    mem_profiler_file = f'{home_path}/mlp_profiler_result/{i}_cpu.txt'
+    mem_profiler_file = f'{home_path}/mlp_profiler_result/orin/{i}_mem.txt'
+    cpu_profiler_file = f'{home_path}/mlp_profiler_result/orin/{i}_cpu.txt'
     
     with open(cpu_profiler_file, 'r') as file:
         cpu_profiler_lines = file.readlines()
     
-    print(cpu_profiler_lines)
+    # print(cpu_profiler_lines)
     with open(mem_profiler_file, 'r') as file:
         mem_profiler_lines = file.readlines()
     
-    print(mem_profiler_lines)
-    # tf_cpu_line_text = profiler_lines[26].split(" ")
-    # tf_cpu_line_text = [x for x in tf_cpu_line_text if x != '']
+    # print(mem_profiler_lines)
     
-    # lite_allocate_tensor_text = profiler_lines[37].split(" ")
-    # lite_allocate_tensor_text = [x for x in lite_allocate_tensor_text if x != '']
+    cpu_line_train_data_line = cpu_profiler_lines[16].split(" ")
+    cpu_line_train_data_line = [x for x in cpu_line_train_data_line if x != '']
+
+    cpu_line_test_data_line = cpu_profiler_lines[17].split(" ")
+    cpu_line_test_data_line = [x for x in cpu_line_test_data_line if x != '']
+
+    mem_train_data_line = mem_profiler_lines[14].split(" ")
+    mem_train_data_line = [x for x in mem_train_data_line if x != '']
+
+    mem_test_data_line = mem_profiler_lines[15].split(" ")
+    mem_test_data_line = [x for x in mem_test_data_line if x != '']
+
+    mem_addr_total_data_line = mem_profiler_lines[16].split(" ")
+    mem_addr_total_data_line = [x for x in mem_addr_total_data_line if x != '']
     
-    # lite_get_input_tensor_text = profiler_lines[38].split(" ")
-    # lite_get_input_tensor_text = [x for x in lite_get_input_tensor_text if x != '']
-    
-    # lite_set_input_data_text = profiler_lines[41].split(" ")
-    # lite_set_input_data_text = [x for x in lite_set_input_data_text if x != '']
-    
-    # lite_invoke_text = profiler_lines[42].split(" ")
-    # lite_invoke_text = [x for x in lite_invoke_text if x != '']
-    
-    # tf_cpu_lines = float(tf_cpu_line_text[2])/5
-    
-    # lite_cpu_lines = float(lite_allocate_tensor_text[2]) 
-    # + float(lite_get_input_tensor_text[2]) 
-    # + (float(lite_set_input_data_text[2])/5) 
-    # + (float(lite_invoke_text[2])/5)
-    
-    # return tf_cpu_lines, lite_cpu_lines
+    return [
+        i,
+        cpu_line_train_data_line[2],
+        cpu_line_test_data_line[2],
+        mem_train_data_line[3],
+        mem_test_data_line[3],
+        mem_addr_total_data_line[1]
+    ]
 
 
 rows = []
@@ -48,4 +49,6 @@ for i in range(1, 6318 + 1):
     rows.append(row)
 
 
-# df = pandas.Dataframe(rows, columns=['idx', 'train(cpu_line)(nano sec)', 'test(cpu_line)(nano sec)', 'train(mem)(MiB)', 'test(mem)(MiB)', 'total_addr_space(mem)(MiB)'])
+df = pd.DataFrame(rows, columns=['idx', 'train(cpu_line)(nano sec)', 'test(cpu_line)(nano sec)', 'train(mem)(MiB)', 'test(mem)(MiB)', 'total_addr_space(mem)(MiB)'])
+
+df.to_csv('cpu_mem_orin.csv', index=False)
